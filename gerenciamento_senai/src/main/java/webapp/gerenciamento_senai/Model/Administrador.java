b0d6cdc24191fd97b787cb97a1c5ff5e5836f99e
Dev_Alberto <albertomeloteco@gmail.com>
2024-06-11 10:48:14 -0300

    ok

--- gerenciamento_senai/src/main/java/webapp/gerenciamento_senai/Model/Administrador.java
+++ gerenciamento_senai/src/main/java/webapp/gerenciamento_senai/Model/Administrador.java
@@ -2,54 +2,25 @@ package webapp.gerenciamento_senai.Model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Entity
+@Getter
+@Setter
 public class Administrador implements Serializable {
 
 // atributos
 @Id
-int cpf;
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+String cpf;
 String nome;
 String email;
 String senha;
-int telefone;
+String telefone;
 
-
-// métodos
-public int getCpf() {
-    return cpf;
-}
-public void setCpf(int cpf) {
-    this.cpf = cpf;
-}
-
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-
-public String getSenha() {
-    return senha;
-}
-public void setSenha(String senha) {
-    this.senha = senha;
-}
-
-public int getTelefone() {
-    return telefone;
-}
-public void setTelefone(int telefone) {
-    this.telefone = telefone;
-}
-    
 }
--- gerenciamento_senai/src/main/java/webapp/gerenciamento_senai/Model/Equipamento.java
+++ gerenciamento_senai/src/main/java/webapp/gerenciamento_senai/Model/Equipamento.java
@@ -2,9 +2,14 @@ package webapp.gerenciamento_senai.Model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Entity
+@Getter
+@Setter
 public class Equipamento implements Serializable {
 
 //  atributos
@@ -15,35 +20,4 @@ String categoria;
 String status;
 
 
-// métodos
-public String getAtivo() {
-    return ativo;
-}
-public void setAtivo(String ativo) {
-    this.ativo = ativo;
-}
-
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-
-public String getCategoria() {
-    return categoria;
-}
-public void setCategoria(String categoria) {
-    this.categoria = categoria;
-}
-
-public String getStatus() {
-    return status;
-}
-public void setStatus(String status) {
-    this.status = status;
-}
-
-
-    
 }
